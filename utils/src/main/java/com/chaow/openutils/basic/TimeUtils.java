5327e52feca4e40ec075783a1349287cc1479210
glassweichao <charles.wei1920@gmail.com>
2019-07-01 18:00:08 +0800

    #update TimeUtils

--- utils/src/main/java/com/chaow/openutils/basic/TimeUtils.java
+++ utils/src/main/java/com/chaow/openutils/basic/TimeUtils.java
@@ -1,7 +1,9 @@
 package com.chaow.openutils.basic;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -19,19 +21,38 @@ public class TimeUtils {
     private TimeUtils() {
     }
 
+    private static SimpleDateFormat getDefaultFormat() {
+        return getFormat(DEFAULT_DATE_FORMAT);
+    }
+
     private static SimpleDateFormat getFormat(String format) {
         String dateFormat = StringUtils.checkEmpty(format, DEFAULT_DATE_FORMAT);
         return new SimpleDateFormat(dateFormat, Locale.getDefault());
     }
 
     public static String getCurrentTime() {
-        return getCurrentTime(null);
+        return getCurrentTime(getDefaultFormat());
     }
 
-    public static String getCurrentTime(@Nullable String dateFormat) {
+    public static String getCurrentTime(@NonNull String dateFormat) {
+        return getCurrentTime(getFormat(dateFormat));
+    }
+
+    public static String getCurrentTime(@NonNull DateFormat dateFormat) {
         long currentTime = System.currentTimeMillis();
-        return getFormat(dateFormat).format(new Date(currentTime));
+        return dateFormat.format(new Date(currentTime));
     }
 
+    public static String millis2String(final long millis) {
+        return millis2String(millis, getDefaultFormat());
+    }
+
+    public static String millis2String(final long millis, @NonNull final String dateFormat) {
+        return millis2String(millis, getFormat(dateFormat));
+    }
+
+    public static String millis2String(final long millis, @NonNull DateFormat dateFormat) {
+        return dateFormat.format(new Date(millis));
+    }
 
 }
